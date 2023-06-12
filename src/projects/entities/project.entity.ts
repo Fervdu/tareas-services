30e7bc8fa6c811c9c6781f47b7f78d0af6fa1883
Fervdu <fvega0920@gmail.com>
2023-06-12 16:34:13 -0500

    creacion de la entidad Project y relacion muchos a uno con Users

--- src/projects/entities/project.entity.ts
+++ src/projects/entities/project.entity.ts
@@ -1,1 +1,23 @@
-export class Project {}
+import { Users } from "src/users/entities/user.entity";
+import { Column, Entity, ManyToOne, PrimaryColumn } from "typeorm";
+
+ @Entity('')
+export class Projects {
+    @PrimaryColumn()
+    id: string
+
+    @Column()
+    name: string
+
+    @Column()
+    image: string
+
+    @Column({ type: 'datetime', default: () => 'CURRENT_TIMESTAMP' })
+    createdAt: Date
+
+    @Column({ type: 'datetime', default: () => null })
+    updatedAt: Date
+
+    @ManyToOne(() => Users, user => user.projects)
+    user: Users
+}
